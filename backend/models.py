6d3cc0d9c449a5dbb6715897647d0597b3813007
kishores0828-dotcom <kishores0828@gmail.com>
2026-05-03 10:37:05 +0530

    Cleaned up all backend import prefixes

--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, Float, ForeignKey
 from sqlalchemy.orm import relationship
-from backend.database import Base # Ensure this path matches your project structure
+import Base # Ensure this path matches your project structure
 
 class User(Base):
     __tablename__ = "users"
